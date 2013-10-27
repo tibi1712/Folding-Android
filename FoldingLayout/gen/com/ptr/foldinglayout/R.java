ef4372be79b86574485218e038390e671933de37
tibi1712 <tibi1712@gmail.com>
2013-10-27 10:34:22 +0200

    Refactor Source Code and suport for drawerlayout and slidingpanelayou

--- FoldingLayout/gen/com/ptr/foldinglayout/R.java
+++ FoldingLayout/gen/com/ptr/foldinglayout/R.java
@@ -9,6 +9,14 @@ package com.ptr.foldinglayout;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static int foldNumber=0x7f010000;
     }
     public static final class drawable {
         public static int ic_launcher=0x7f020000;
@@ -44,4 +52,33 @@ public final class R {
          */
         public static int AppTheme=0x7f040001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a FoldingMenu.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #FoldingMenu_foldNumber com.ptr.foldinglayout:foldNumber}</code></td><td></td></tr>
+           </table>
+           @see #FoldingMenu_foldNumber
+         */
+        public static final int[] FoldingMenu = {
+            0x7f010000
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.ptr.foldinglayout.R.attr#foldNumber}
+          attribute's value can be found in the {@link #FoldingMenu} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.ptr.foldinglayout:foldNumber
+        */
+        public static final int FoldingMenu_foldNumber = 0;
+    };
 }
